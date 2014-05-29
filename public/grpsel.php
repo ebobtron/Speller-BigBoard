62fbf67c5b04f7318db2b288f8d6a836b6205444
ebobtron <erobclark@att.net>
2014-05-28 23:02:26 -0500

    some research cookie code

--- public/grpsel.php
+++ public/grpsel.php
@@ -1,20 +1,31 @@
 
 
 <?php 
-
+    
+    $cookie_name = 'leaderboard_cookie';
+    $cookie_time = time() + (24 * 60 * 60 * 365); // about a year
+    
+    if(isset($_POST['group'])) {
+        
+        $cookie_value = $_POST['group'];
+        setcookie($cookie_name, $cookie_value, $cookie_time, '/');                
+    }
+    
+    if(isset($_COOKIE[$cookie_name])) {
+        
+        echo $_COOKIE[$cookie_name]."<br>";
+        print_r($_COOKIE);
+    }
             
     $template = "grpselform.php";
     
             // render header
             require("../template/header.php");
     
-    print_r($_POST);
-    
             // render template
             require("../template/$template");
 
             // render footer
             require("../template/footer.php"); 
-
 
 ?>
