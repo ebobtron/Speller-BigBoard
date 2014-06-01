a58ba1852a2c379b9f0162ae1c930929a0bee166
ebobtron <erobclark@att.net>
2014-06-01 13:39:47 -0500

    added group title, removed blockquote tag, added hidden input tag 'grp' changed <br> to <br /> ???

--- template/submitform.php
+++ template/submitform.php
@@ -9,33 +9,35 @@
 *************************************************************/
 -->
 
-
 <div class="gentext">
 
-  <blockquote style="font-size:20">
-    <h2> file submission continued </h2>
-  </blockquote>
+  <h2> file submission continued </h2>
+  
 
   <?php 
+    
+    echo " group: ",$title,"<br />";
 
-    echo " submitters name: ",$name,"<br>";
-    echo " this submissions id: ",$id,"<br>"; 
+    echo " submitters name: ",$name,"<br />";
+    echo " this submissions id: ",$id,"<br />"; 
     echo " submitters email: ",$_POST['email']; 
 
     echo "
         <form enctype='multipart/form-data' action='uploadfile.php' method='POST'>
             <input type='hidden' name='MAX_FILE_SIZE' value='25000'>
 
-            <p>Please send <h3> \"speller\"</h3> not speller.c, thank you.<br><br>
+            <p>Please send <h3> \"speller\"</h3> not speller.c, thank you.<br /><br />
                Select File to Upload: <input type='file' name='uploadedfile'></p>
 
             <input type='hidden' name='name' value='",$name,"'>
-            <input type='hidden' name='email' value='",$_POST['email'],"'>
+            <input type='hidden' name-'grp' value='",$grp,"'>
             <input type='hidden' name='id' value='",$id,"'>
+            <input type='hidden' name='email' value='",$_POST['email'],"'>
             <input type='submit' value='submit' style='font-size:20px'>
         </form>";
   ?>
 
+<br />
 
 </div>
 
