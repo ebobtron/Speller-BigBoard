389b5c7109de4cc8b1d292988ca52ae5ff7b882c
ebobtron <erobclark@att.net>
2014-06-07 19:59:13 -0500

    removed $id as displayed and hidden value embedded the PHP into the HTML for this form

--- template/submitform.php
+++ template/submitform.php
@@ -1,49 +1,44 @@
 <!--
-/***
+***
 *
 *   submitform.php  user interfaces to upload file
 *
 *   Robert Clark, aka ebobtron
 *   CS50x final project   winter/spring 2014  with Launch Code
 *
-*************************************************************/    -->
-
+*************************************************************    -->
 
 <div class="gentext">
   
   <h2> file submission continued </h2>
     
-    group: <? echo $title; ?><br />
-    submitters name: <? echo $name; ?><br />
-    submitters email: <? echo $_POST['email']; ?><br />
-<br />
-<br />
-
+    group: <?=$title?><br>
+    submitters name: <?=$name?><br>
+    submitters email: <?=$_POST['email']?><br>
+    <br>
+    <br>
 
 <!--    
 // submission Id will be issued when valid data added to table
-#    echo " this submissions id: ",$id,"<br />";  --> 
+    echo " this submissions id: ",$id,"<br />";  --> 
     
-<?php
-    echo "
-        <form enctype='multipart/form-data' action='uploadfile.php' method='POST'>
-            <input type='hidden' name='MAX_FILE_SIZE' value='25000'>
 
-            Please send <h3> \"speller\"</h3> not speller.c, thank you.<br /><br />
-            Select File to Upload: <input type='file' name='uploadedfile'><br />
-            <br />
+    <form enctype='multipart/form-data' action='uploadfile.php' method='POST'>
+        <input type='hidden' name='MAX_FILE_SIZE' value='25000'>
 
-            <input type='hidden' name='name' value='",$name,"'>
-            <input type='hidden' name='group' value='",$group,"'>
-            <input type='hidden' name='id' value='",$id,"'>
-            <input type='hidden' name='email' value='",$_POST['email'],"'>
+        Please upload <h3> "speller" </h3> not speller.c, thank you.<br>
+        <br>
+        Click Browse to Select File to Upload: <input type='file' name='uploadedfile'><br>
+        <br>
 
-            <input type='submit' value='submit' style='font-size:20px'>
+        <input type='hidden' name='name' value='<?=$name?>'>
+        <input type='hidden' name='group' value='<?=$group?>'>
+   <!-- <input type='hidden' name='id' value='",$id,"'>     -->
+        <input type='hidden' name='email' value='<?=$_POST['email']?>'>
         
-        </form>";
-  ?>
-
-<br />
+        <input type='submit' value='submit' style='font-size:20px'>
+        
+    </form>
+    <br>
 
 </div>
-
