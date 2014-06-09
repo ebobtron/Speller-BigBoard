5c782718969a5ab08d16b05cf4aeef7564d8f414
ebobtron <erobclark@att.net>
2014-06-08 23:59:22 -0500

    make "no cookie" default group LaunchCode w 2014  for now

--- public/show.php
+++ public/show.php
@@ -16,11 +16,17 @@
     // Check if cookie contains group value
     // if cookie exist, set group var based on cookie data
     if (isset($_COOKIE['leaderboard_cookie'])) {
+        
         $group = $_COOKIE['leaderboard_cookie'];
     }
     // else, redirect to group selection page
     else {
-        header("Location: http://" . $_SERVER["HTTP_HOST"] . dirname($_SERVER["PHP_SELF"]) . "/grpsel.php");
+        
+        // temp default group
+        $group = "lcstlw2014";
+        
+        #header("Location:".
+        #"http://".$_SERVER["HTTP_HOST"].dirname($_SERVER["PHP_SELF"])."/grpsel.php");
     }
     
     // tell the user which group they are looking at
@@ -28,7 +34,6 @@
     $head = $headString[$group];
     $link = $linkString[$group];
     
-
     // get table rows from display
     $rows = getPut("rows", getGroupNumber($group));
 
