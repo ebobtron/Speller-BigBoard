1a9350c1a78929123cc2591f79b8daaf7c4b4941
ebobtron <erobclark@att.net>
2014-06-14 16:50:51 -0500

    this is part of the sites pages

---
+++ include/config.php
@@ -0,0 +1,19 @@
+<?php
+  
+  // configuration file for leaader board
+
+  // passwords and user names removed.
+
+  // important data for email funtion
+  $host = "mbox.freehostia.com";
+  $port = "2525";
+  $pwd = "xxxxx";
+  $username = "leaderboard@avsfilmcutter.com";
+  $bcc = "xxxxxxxxxx";
+
+  //  data for database access
+  $dbpath = "localhost";
+  $dbuser = "XXXXXXXX";
+  $dbpass = "XXXX";
+
+?>
