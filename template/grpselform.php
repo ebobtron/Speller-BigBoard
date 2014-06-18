319421e072704da86a28006e933a851afb916080
ebobtron <erobclark@att.net>
2014-06-17 23:05:15 -0500

    css changes and content changes for this form

--- template/grpselform.php
+++ template/grpselform.php
@@ -1,31 +1,35 @@
 
 
-<div class="grpselect">
+<div class="gentext lilmar" style='width:80%'>
 
+  <h3 class="head lilmar"> Group Selection </h3>
+    Please click the image below that best describes your circumstances, thank you.<br>
+    <ul><li>
+    If you have a group not shown here please contact the
+    <a href="mailto:ebobtron@aol.com" class="head-links" style='font-size:19px'>
+    Leader Board Administrator.</a>&nbsp;
+    We can add your group.
+</div>
 
-    <h3> Group Selection </h3>
-        Please click the image below that best describes your circumstances, thank you.<br>
-  <br>
-  &nbsp;&nbsp;please click the group you belong to
-  
-  <form action="grpsel.php" method="post">
+<div class="grpSelect">  
+<form action="grpsel.php" method="post">
   
   <table id="grpTable">
   <tr>
     <td class='grptd'>
       <div id="coderg">
-        <button id="coderggrp" name='group' value="codergirlstl2014">
+        <button id="coderggrp" name='group' value="codergirlstl2014" title="click for Coder Girl STL 2014">
          <img alt="click for this group" id='codergimg' src="../public/img/coder-g.jpg">
            &nbsp;STL&nbsp;&nbsp; 2014
         </button>
       </div>
     </td>
-    <td>If you are studying with groups accsosiated with Coder Girl STL</td>
+    <td>If you are studying with groups associated with Coder Girl STL</td>
   </tr>
   <tr>
     <td class="grptd">
       <div id="lcode">
-        <button id="lcodegrp" name='group' value="lcstlw2014">
+        <button id="lcodegrp" name='group' value="lcstlw2014" title="click for LaunchCode winter 2014">
          <img alt="click for this group" id='lcodeimg' src="../public/img/logo-white-lc.png">
      &nbsp;STL&nbsp;&nbsp;winter 2014
         </button>
@@ -36,19 +40,26 @@
   <tr>
     <td class="grptd">
       <div id="edx">      
-        <button id="edxgrp" name='group' value="edx2014">
+        <button id="edxgrp" name='group' value="edx2014" title="click for edX.org 2014">
           <img alt="click for this group" id='edximg' src="../public/img/hero-logo-edx.png">
           <img alt="click for this group" id='cs50img' src="../public/img/cs50_299x188.jpg">
         </button>
       </div>
     </td>
-    <td>If your studying CS50x on your own with edX.org. If you have a group not shown here please contacted the <a href="mailto:ebobtron@aol.com" class="head-links">Leader Board Administrator.</a></td>
+    <td>If your studying CS50x on your own with edX.org 
+    </td>
   </tr>
-  </table>
+  <tr>
+   <td>your group here</td>
+   <td>contact 
+     <a href="mailto:ebobtron@aol.com" class="head-links" style='font-size:19px'>
+     Leader Board Administrator.</a>&nbsp;
+     We can add your group.
+   </td>
+  </tr>
+</table>
   
-  </form>
+</form>
 
-  <br />
-  
-</div>
+</div><br>
 
