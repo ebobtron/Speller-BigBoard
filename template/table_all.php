11b4db8fd0b7fd1f66fa2f0f066c8895dbc6d3b8
ebobtron <erobclark@att.net>
2014-06-17 23:00:18 -0500

    enable "Group", style changes, rework and resize all groups display

--- template/table_all.php
+++ template/table_all.php
@@ -11,99 +11,79 @@
 
 <div class="lnk">
 
-<p style="display: inline;">
+    <p style="display: inline;">
 
-  <!-- $_GET  ? r = gt means a group table -->
-  <a class="menu" href="../public/grpsel.php?r=ta" title="click to submit your speller">
-        Submit Your Program&nbsp;</a></p>
+    <!-- $_GET  ? r = gt means a group table -->
+    <a class="menu" href="../public/grpsel.php?r=ta" title="click to submit your speller">
+        Submit Your Program&nbsp;</a>
+    </p>
 
 
-&middot;
+    &middot;
 
-<p style="display: inline;">
-<a class="menu" href="criteria.php">&nbsp;Test Criteria&nbsp;</a></p>
+    <p style="display: inline;">
+        <a class="menu" href="criteria.php">&nbsp;Test Criteria&nbsp;</a>
+    </p>
 
-&middot;
+    &middot;
 
-<p style="display: inline;">
-<a class="menu hover" href="download.php">&nbsp;Download "test"&nbsp;</a></p>
+    <p style="display: inline;">
+        <a class="menu hover" href="download.php">&nbsp;Download "test"&nbsp;</a>
+    </p>
 
-&middot;
+    &middot;
 
-<p style="display: inline;">&nbsp;Groups
-<!--
-<a class="menu hover" href="">&nbsp;Download "test"&nbsp;</a>
--->
-</p>
+    <p style="display: inline;">
+
+        <a class="menu hover" href="grpsel.php">&nbsp;Groups</a>
+    </p>
 
 </div>
 
 <div class="div-table">
 
-<h3 style="margin:0px">all times in seconds</h3>
+  <h3 style="margin:0px">all times in seconds</h3>
 
-<table class="table">
- <thead class="thead">
-  <tr>
-	 <th class="th"></th>
-	 <th class="th">name</th>
-	 <th class="th">total time</th>
-   <th class="th" style='width:40%'>group</th>
-
- 
-  </tr>
-</thead>
-<tbody class="tbody ldbd-tbody">
+  <table class='table-all'>
+    <thead class="thead">
+      <tr>
+        
+        <th class="th">name</th>
+        <th class="th">total time</th>
+        <th class="th" style='width:50%'>group</th>
+      </tr>
+    </thead>
+    <tbody class="tbody ldbd-tbody">
 
 <?php
-  $loop = 0;
-	foreach($rows as $row)
-	{
-		if($loop % 2 == 0)
-		  echo " <tr class='row'> ";
-		else
-		  echo " <tr> ";
-    
-    $keyNum = $row['grp'];  	
-    $groupName = $titleString[$keys[$keyNum]];
-		
-    $grp = sprintf("%8s", $groupName);
-    
-		$to = sprintf("%0.4f", $row['total']);
 
+    $loop = 0;
 
-		echo"
-    <td class='td'></td>
-		<td class='td'>",$row['name'],"</td>
-		<td class='td'>",$to,"</td>
-    <td class='td' style='width:30%;font-family:Arapey_Italic;font-size:14pt'>",$grp,"</td>
-    <td class='td'></td>
+    foreach($rows as $row){
 
-		</tr>";
+        if($loop % 2 == 0)
+            echo " <tr class='row'> ";
+        else
+            echo " <tr> ";
 
-		$loop++;
-	}
-	/*
-    <th class="th">load time</th>
-	  <th class="th">check time</th>
-    <th class="th">size time</th>
-	  <th class="th">unload time</th>
-	  <th class="th">heap used</th>
-    
-    $ld = sprintf("%0.4f", $row['dload']);
-		$ck = sprintf("%0.4f", $row['tcheck']); 
-		$sz = sprintf("%0.4f", $row['size']);
-		$ul = sprintf("%0.4f", $row['unload']);
-		$mm = sprintf("%0.4f MB", $row['mem']);
-      
-  	<td class='td'>",$ld,"</td>
-		<td class='td'>",$ck,"</td>
-    <td class='td'>",$sz,"</td>
-		<td class='td'>",$ul,"</td>
-		<td class='td right'>",$mm,"</td>
-  */  
+        $total = sprintf("%0.4f", $row['total']);
+        
+        $keyNum = $row['grp'];  	
+        $groupName = $titleString[$keys[$keyNum]];
+
+        $grp = sprintf("%8s", $groupName);
+
+        echo"
+            <td class='td-all'>", $row['name'], "</td>
+            <td class='td-all'>", $total, "</td>
+            <td class='td td-allgrp'>", $grp, "</td>
+            </tr>
+            ";
+
+        $loop++;
+    }
+  
 ?>
-      </tbody>
-	  </table>
-  </div>
-<br>
+    </tbody>
+  </table>
+</div><br>
