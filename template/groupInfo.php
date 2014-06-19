8d33cc841f08d931d72fd806f8e5c7bf75433800
ebobtron <erobclark@att.net>
2014-06-18 22:28:29 -0500

    new navigation form template page view other groups

---
+++ template/groupInfo.php
@@ -0,0 +1,30 @@
+
+<div class="gentext lilmar" style='width:80%'>
+
+  <h3 class="head lilmar">Study Group Info for CS50x Problem Set 6: Mispellings or Misspellings</h3>
+    Select a link below to view individual group results on the leader board.<br>
+    <ul>
+      <li>
+        <a href="show.php?grp=1" class="head-links" style='font-size:19px'>
+           LaunchCode Saint Louis winter 2014</a></li>
+        
+      <li>
+        <a href="show.php?grp=2" class="head-links" style='font-size:19px'>
+           Coder Girl Saint Louis 2014</a></li>
+        
+      <li>
+        <a href="show.php?grp=3" class="head-links" style='font-size:19px'>
+           edX.org 2014</a></li>
+           
+      <li>  
+        <a href="show.php?grp=0" class="head-links" style='font-size:19px'>
+           All Submissions to Board in One Table</a></li>
+              
+    </ul>
+    
+    
+    If you have a group not shown here please contact the
+    <a href="mailto:ebobtron@aol.com" class="head-links" style='font-size:19px'>
+    Leader Board Administrator.</a>&nbsp;
+    We can add your group.
+</div>
