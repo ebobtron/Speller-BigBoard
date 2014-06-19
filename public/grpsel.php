b18bf3fcb0663f23489fd3254a66e2a4f394182e
ebobtron <erobclark@att.net>
2014-06-18 22:25:47 -0500

    removed tab characters, change navigation flow for a redirect

--- public/grpsel.php
+++ public/grpsel.php
@@ -2,57 +2,51 @@
 <?php
 /**
 *
-*   grpsel.php  group selection controler 
-* 
-*   Robert Clark, aka ebobtron et al.
-*   
-*   An expansion of my 
+*   grpsel.php  group selection controler
+*
+*   Robert Clark, aka ebobtron et al
+*
+*   An expansion of my
 *   CS50x final project   winter/spring 2014 with Launch Code
-* 
+*
 ***************************************/
-    
+
     error_reporting(E_ALL);
-    
-    if(isset($_GET['r'])) {
-        
-        if($_GET['r'] == 'gt') {
-    
-            header("Location:"."getspeller.php");
-        }
-    }
     
     $cookie_name = 'leaderboard_cookie';
     $cookie_time = time() + (24 * 60 * 60 * 365); // about a year
     
+    
+    if(!isset($_GET['chg'])) {
+
+        if(isset($_COOKIE[$cookie_name])) {
+                
+            header("Location:"."getspeller.php");
+        }
+    }
+
     if(isset($_POST['group'])) {
-        
+
         $cookie_value = $_POST['group'];
         setcookie($cookie_name, $cookie_value, $cookie_time, '/');
         header("Location:".
-        "http://".$_SERVER["HTTP_HOST"].dirname($_SERVER["PHP_SELF"])."/getspeller.php");                
+        "http://".$_SERVER["HTTP_HOST"].dirname($_SERVER["PHP_SELF"])."/getspeller.php");
     }
-    
-    
+
+
     /* // diagnostic code
-    if(isset($_COOKIE[$cookie_name])) {
-        
+    if(isset($_COOKIE[$cookie_name])) {    
         echo $_COOKIE[$cookie_name]."<br>";
         print_r($_COOKIE);
     }
     ********************************/
-    
-    if(isset($_GET['grouplink'])) {
-        
-        
-    
-    
-    }
-            
+
+
     $template = "grpselform.php";
     
             // render header
             require("../template/header.php");
-    
+
             // render template
             require("../template/$template");
 
