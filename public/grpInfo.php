fed514c12048733b7d33765448f109f000983d27
ebobtron <erobclark@att.net>
2014-06-18 22:27:11 -0500

    new navigation page controller to view other groups or all

---
+++ public/grpInfo.php
@@ -0,0 +1,24 @@
+
+
+<?php 
+
+
+    $template = "groupInfo.php"; 
+        
+        // render header
+        require("../template/header.php");
+
+        // render template
+        require("../template/$template");
+
+        // render footer
+        require("../template/footer.php");
+
+
+
+
+
+
+
+
+?>
