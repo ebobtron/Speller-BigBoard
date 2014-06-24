7a817ba067837902528e1bd9bc03776b55a11b07
ebobtron <erobclark@att.net>
2014-06-24 16:43:08 -0500

    put the magic words out of sight

--- include/config.php
+++ include/config.php
@@ -1,19 +1,19 @@
 <?php
+	
+	// configuration file for leaader board
+	
+	// important data for email funtion
+	$host = "xxxx.freehostia.com";
+  $port = "xxxx";
+	$pwd = "cs50x";
+	$username = "leaderboard@avsfilmcutter.com";
+  $bcc = "xxxxxxxxx";
+	
+	$dbpath = "localhost";
+	$dbuser = "xxxxxxxxxxxxxx";
+	$dbpass = "xxxx";
   
-  // configuration file for leaader board
-
-  // passwords and user names removed.
-
-  // important data for email funtion
-  $host = "mbox.freehostia.com";
-  $port = "2525";
-  $pwd = "xxxxx";
-  $username = "leaderboard@avsfilmcutter.com";
-  $bcc = "xxxxxxxxxx";
-
-  //  data for database access
-  $dbpath = "localhost";
-  $dbuser = "XXXXXXXX";
-  $dbpass = "XXXX";
+  $magicword = "xxxx";
+  $magicword_2 = "xxxxxx";
 
 ?>
