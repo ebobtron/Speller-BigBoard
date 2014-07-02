a4a7d0f622deddce500844463ebbcccbd0b57420
ebobtron <erobclark@att.net>
2014-07-02 14:19:00 -0500

    issue 21, implement saniTize where applicable

--- public/grpsel.php
+++ public/grpsel.php
@@ -1,22 +1,23 @@
 
 <?php
-/**
-*
-*   grpsel.php  group selection controler
-*
-*   Robert Clark, aka ebobtron et al
-*
-*   An expansion of my
-*   CS50x final project   winter/spring 2014 with Launch Code
-*
-***************************************/
+/*
+ *
+ *  grpsel.php  group selection controler
+ *
+ *  Robert Clark, aka ebobtron et al
+ *
+ *  An expansion of my
+ *  CS50x final project   winter/spring 2014 with Launch Code
+ *
+ **************************************************************/
 
-    error_reporting(0);
+    error_reporting(0); // E_ALL
+    
+    require("../include/helfun.php");
     
     $target = "sub";
     $cookie_name = 'leaderboard_cookie';
     $cookie_time = time() + (24 * 60 * 60 * 365); // about a year
-    
     
     if(!isset($_GET['chg'])) {
 
@@ -27,15 +28,15 @@
     }
     else {
         
-        if($_GET['chg'] === "default") {
-            
+        if(saniTize($_GET['chg']) === "default") {
+
             $target = "chggrp";
         }
     }
 
     if(isset($_POST['group'])) {
 
-        $cookie_value = $_POST['group'];
+        $cookie_value = saniTize($_POST['group']);
         setcookie($cookie_name, $cookie_value, $cookie_time, '/');
         header("Location:".
         "http://".$_SERVER["HTTP_HOST"].dirname($_SERVER["PHP_SELF"])."/getspeller.php");
@@ -43,29 +44,21 @@
     
     if(isset($_POST['target'])) {
         
-        if($_POST['target'] === "chggrp") {
+        if(saniTize($_POST['target']) === "chggrp") {
             
             header("Location:"."show.php");
         }
     }
     
-    /* // diagnostic code
-    if(isset($_COOKIE[$cookie_name])) {    
-    echo $_COOKIE[$cookie_name]."<br>";
-    print_r($_COOKIE);
-    }
-    ********************************/
-
-
     $template = "grpselform.php";
     
-            // render header
-            require("../template/header.php");
+        // render header
+        require("../template/header.php");
 
-            // render template
-            require("../template/$template");
+        // render template
+        require("../template/$template");
 
-            // render footer
-            require("../template/footer.php"); 
+        // render footer
+        require("../template/footer.php"); 
 
 ?>
