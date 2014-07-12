f2f9c8ad46892d75dbb4fea5f5ca476e8e2a7af4
ebobtron <erobclark@att.net>
2014-07-12 18:53:40 -0500

    custom 403 page

---
+++ public/403.php
@@ -0,0 +1,10 @@
+
+
+    <!-- this is forbidden  402.html -->
+    <?php require("../template/header.php"); ?>
+
+    <div class="gentext">
+    <h2> Sorry this is forbidden</h2>
+    </div> 
+
+    <?php require("../template/footer.php"); ?>
