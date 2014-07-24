aef4e95f69b2fd8059863e42fdd982057c327687
ebobtron <erobclark@att.net>
2014-07-24 18:31:11 -0500

    more footer fixes.

--- template/footer.php
+++ template/footer.php
@@ -6,11 +6,11 @@
     <td style="width:5%">
     </td>
     <td>
-      <a href="validator.w3.org/check/referer">
+      <a href="http://validator.w3.org/check/referer">
         <img src="../public/img/HTML5.png" alt="HTML 5"
             style="border:0;width:24px"></a>
-             
-      <a href="jigsaw.w3.org/css-validator/check/referer">
+
+      <a href="http://jigsaw.w3.org/css-validator/check/referer">
         <img style="border:0;width:66px;height:24px"
             src="http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="Valid CSS!"></a>
     </td>
@@ -23,7 +23,7 @@
     </td>
     <td>
       <a class="legal-links"
-         href="http://trademark.harvard.edu/pages/trademark-notice">
+         href="http://trademark.harvard.edu/pages/trademark-notice/">
         "Harvard" is &copy; 2014 The President and Fellows of Harvard College
       </a>
       <br />
@@ -36,6 +36,6 @@
     </td>
   </tr>
 </table>
-  
+
 </body>
 </html>
