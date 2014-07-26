82b9edc7e450db3068bc64c053c14803e3c8c5cb
ebobtron <erobclark@att.net>
2014-07-26 02:02:09 -0500

    #27 added the radio buttons and the fieldset

--- template/submitform.php
+++ template/submitform.php
@@ -17,17 +17,25 @@
     submitters name: <?=$name?><br />
     submitters email: <?=$_POST['email']?><br />
     <br />
-    <br />
 
     <form enctype='multipart/form-data' action='uploadfile.php' method='POST'>
         <input type='hidden' name='MAX_FILE_SIZE' value='48000' />
 
-        Please upload <h3> "speller" </h3> not speller.c, thank you.<br />
+        Please upload <big><big> "speller"</big></big> not speller.c, thank you.<br />
         <br />
-        <big><b>Click Browse to Select File to Upload:</b></big>
+        <big><b>Click Browse to Select File to Upload:</b></big><br />
             <input type='file' name='uploadedfile' /><br />
         <br />
-
+        
+        <fieldset style="border:2px solid #3399ff;width:50%;border-radius: 10px">
+        <legend>&nbsp;Type Data Structure Used&nbsp;</legend>
+        <input type="hidden" name="type" value="0">
+        <input type="radio" name="type" value="1">Hash Table
+        <input type="radio" name="type" value="2">Trie
+        <input type="radio" name="type" value="3">Other
+        </fieldset>
+        <!--  -->
+        <br />
         <input type='hidden' name='name' value='<?=$name?>' />
         <input type='hidden' name='group' value='<?=$group?>' />
         <input type='hidden' name='email' value='<?=$_POST['email']?>' />
