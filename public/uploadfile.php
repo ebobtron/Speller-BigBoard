b4341ece74406f7fe5fe8737e27efa91df5082d0
ebobtron <erobclark@att.net>
2014-07-26 02:06:36 -0500

    #27 added data type to group number 2 is now 2-2 (group 2 trie)

--- public/uploadfile.php
+++ public/uploadfile.php
@@ -1,7 +1,6 @@
 
 <?php
 /*
- *
  *   uploadfile.php  manage the uploaded file
  *
  *   Robert Clark, aka ebobtron et al.
@@ -12,38 +11,44 @@
  ***************************************************************/
 
     error_reporting(0); // E_ALL
-
-    require "../include/helfun.php";
     
-    $dirString = "../uploading/";
-    
-    if(file_exists("../minis/alt_load.txt")) {
+    if($_POST['type'] == 0){
         
-        $dirString = "../uploading_alt/";
+        header("Location:"."alert.php?type=no");
+        exit;   
     }
     
+    require "../include/helfun.php";
+
+    $dirString = "../uploading/";
+
+    if(file_exists("../minis/alt_load.txt")) {
+
+        $dirString = "../uploading_alt/";
+    }
+
     if($_FILES['uploadedfile']['name'] !== "speller") {
-       
+
        $message = "bogus or corupted file submission \"".
                   $_FILES['uploadedfile']['name'].
                   "\" plesse submit the file \"speller\" again.";
        $message = $message . "<br /><br /><b>No file submission.</b>";          
     }
     else {
-    
+
         $group = saniTize($_POST['group']);
-    
+
         $title = $titleString[$group];
         $head = $headString[$group];
         $link = $linkString[$group];
-        
+
         $name = saniTize($_POST['name']);
         $email = validEmail($_POST['email']);
-     
+
         // build the target path string and some useful other strings
-        $submissionNameGp = $name . getGroupNumber($group);
+        $submissionNameGp = $name . getGroupNumber($group) . '-' . $_POST['type'];
         $fileName = basename($_FILES['uploadedfile']['name']);
-    
+
         $target_path = $dirString . $submissionNameGp;
         $target_path = $target_path . $fileName . ".x";
         
@@ -52,17 +57,17 @@
             header("Location:"."alert.php?dupSub=yes");
             exit;
         }
-        
+
         // if file is is up move to defined folder
         $success = move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path);
         
-        
         // if successful and if not
         if($success) {
-
+            
+            $grp_type = getGroupNumber($group) . '-' . $_POST['type']; 
             // create subinfo file for automation
-            createSubInfo($name, getGroupNumber($group), $email, $dirString);
-        
+            createSubInfo($name, $grp_type, $email, $dirString);
+
             // build message and email strings
             $message = "The file \" " . basename($_FILES['uploadedfile']['name']);
             $message = $message . " \" has been uploaded <br /> for submitter: \" ";
@@ -72,11 +77,10 @@
             $message = $message . "for questions contact the administrator <a href=\"";
             $message = $message . "mailto:ebobtron@aol.com\">mailto:ebobtron@aol.com</a>".
                                   "<br /><br />";
-        
+
             $emailBody = "Received a submission from:  " . $name .
                          "  Group:  ". $title . "  Contact:  " . $email . "\r\n";
 
-        
         }
         else {
 
@@ -85,7 +89,7 @@
             $message = $message . "for questions contact the <a href=\"";
             $message = $message . "mailto:ebobtron@aol.com\">".
                                   "Leader Board Administrator</a><br />";
-        
+
             $emailBody = "a submission failed to upload from:  ";
             $emailBody = $emailBody . $name . " @ " . $email;
         }
