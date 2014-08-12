0cf8aa44d87cc0542463e92fa48f004af1aac7c3
ebobtron <erobclark@att.net>
2014-08-12 17:05:21 -0500

    text changes added contact info

--- template/aboutus_view.php
+++ template/aboutus_view.php
@@ -2,7 +2,7 @@
 <div class="gentext">
 
     <h3 class="lilmar">About us</h3>
-    
+
     We are students of CS50x offered by edX.org.&nbsp; This, is an expanded version of my
     CS50 final project.&nbsp; The project uses elements of the following.
     <div class="grpSelect" style="width:100%">
@@ -10,8 +10,8 @@
         <li>Hypertext Markup Language (HTML)
         <li>Cascading Style Sheets (CSS)
         <li>PHP: Hypertext Preprocessor and Java Script Languages
-        <li>PHP Data Objects (PDO) for connection to a MySQL data base
-        <li>MySQL data base
+        <li>PHP Data Objects (PDO) for connection to a MySQL database
+        <li>MySQL database
         <li>JavaScript Object Notation (JSON) file by Chris Hinds
     </ul>    
     for automation of submission possessing
@@ -20,8 +20,10 @@
         <li>CURL
         <li>FTP
         <li>BASH Scripting
-        
+    </ul>
+    <a href="mailto:ebobtron@aol.com" class="head-links" style="font-size:1.3em">
+    email the administrator</a>   
     </div>     
-
+    
 
 </div>
