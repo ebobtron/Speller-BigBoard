236a2f06fc6a88705f8dd0e6b439ced4b5bc6457
ebobtron <erobclark@att.net>
2014-08-21 09:43:55 -0500

    so icons for the people

--- template/header.php
+++ template/header.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE HTML>
 <!-- written by Robert Clark, et al. see public/aboutus.php -->
 <html lang="en">
@@ -6,12 +5,13 @@
   <head>
 
     <meta http-equiv="Content-type" content="text/html;charset=utf-8" />
-    <meta name="description" content="Pset6 leader board for students
-    of CS50/CS50x. Supported by students of CS50x" />
+    <meta name="description" content="Pset6, Mispellings, leader board for students
+    of CS50  CS50x. Supported by students of CS50x" />
     
     <link href="../public/css/leaderboard.css" rel="stylesheet" />
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
     <link rel="icon" href="favicon.ico" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
     
     <style type="text/css"></style>
 
