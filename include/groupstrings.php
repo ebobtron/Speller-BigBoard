69da4ee7aec7a19581478a13c1abd4a12fb62f8b
ebobtron <erobclark@att.net>
2015-01-22 22:02:53 -0600

    changes for use in 2015

--- include/groupstrings.php
+++ include/groupstrings.php
@@ -1,46 +1,60 @@
+<?php
+/*
+ *  groupstrings.php
+ *   
+ *  group definitions for titles and links
+ *
+ *  an expansion of my 
+ *  CS50x final project winter/spring 2014  with Launch Code
+ *
+ *  copyright 2015 Robert Clark  et al.
+ *
+ **************************************************************************/
 
-<?php 
-
-    /*** 
+   /** 
      * changed to an associative array to improve readability for future and 
      * prevent need to call lookup function to get group number
      * 
      * Chris
-     */  
+     *
+     ***************/
 
     $validGrpNum_R = array(0,1,2,3);
 
-    $defaultString = "edx2014";
+    $defaultString = "edx2015";
     
     $titleString = array(
            "cs50staff" => "CS50 Staff Solution",
-           "lcstlw2014" => "LaunchCode STL winter 2014",
-           "codergirlstl2014" => "Coder Girl STL 2014",
-           "edx2014" => "edX.org 2014"
+           "lcstlw2015" => "LaunchCode STL winter 2015",
+           "codergirlstl2015" => "Coder Girl STL 2015",
+           "edx2015" => "edX.org 2015"
            );
     
     
     $headString = array(
            "cs50staff" => "<br>", 
-           "lcstlw2014" => "&nbsp;-&nbsp;&nbsp;-&nbsp;&nbsp;".
+           "lcstlw2015" => "&nbsp;-&nbsp;&nbsp;-&nbsp;&nbsp;".
                "LaunchCode St. Louis Edition<br>",
-           "codergirlstl2014" => "&nbsp;-&nbsp;&nbsp;-&nbsp;&nbsp;".
+           "codergirlstl2015" => "&nbsp;-&nbsp;&nbsp;-&nbsp;&nbsp;".
                "<a href='mailto:codergirl@launchcode.org' class='head-links'>".
                "Coder Girl STL Edition</a><br>",
-           "edx2014" => "&nbsp;-&nbsp;&nbsp;-&nbsp;&nbsp;edX.org Edition<br>"
+           "edx2015" => "&nbsp;-&nbsp;&nbsp;-&nbsp;&nbsp;edX.org Edition<br>"
            );
     
     
     $linkString = array(
            "cs50staff" => "",
-           "lcstlw2014" => "winter 2014 with ".
+           "lcstlw2015" => "winter 2015 with ".
                "<a href='http://launchcode.org' class='head-links'>". 
                "LaunchCode.org</a>",
-           "codergirlstl2014" => "2014 with ".
+           "codergirlstl2015" => "2015 with ".
                "<a href='http://launchcode.org' class='head-links'>". 
                "LaunchCode.org</a>",
-           "edx2014" => ""
+           "edx2015" => "2015 with <a href='http://edx.org' class='head-links'>".
+               "edX.org</a>"
            );
-                                        
-                                        
+
+/*   last edit: 01/21/2015  ebt   */                            
 ?>
+
+
