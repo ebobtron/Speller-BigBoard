2e7fff187f381eff39faac61199004160ec418ec
ebobtron <erobclark@att.net>
2015-01-22 22:15:41 -0600

    redirect submissions link during suspension

--- template/links.php
+++ template/links.php
@@ -3,8 +3,12 @@
 
     <p style="display: inline;">
     <!-- $_GET  ? r = gt means a group table -->
+    <!--
         <a class="menu" href="../public/grpsel" title="click to submit your speller">
            Submit Your Program&nbsp;</a>
+           -->
+           <a class="menu" href="../public/suspend" title="submissions suspended for 2014">
+           <strike>Submit Your Program</strike>&nbsp;</a>
     </p>
 
     &middot;
@@ -26,3 +30,4 @@
     </p>
 
 </div>
+<? /*   last edit: 01/21/2015  ebt   */ ?>
