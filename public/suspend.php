40d3c5f5a76db800d150e0831f5bffe1d3d1ad62
ebobtron <erobclark@att.net>
2015-01-22 22:11:06 -0600

    submission suspend notice

---
+++ public/suspend.php
@@ -0,0 +1,30 @@
+<?php 
+/*
+ *  suspend.php
+ *
+ *  an expansion of my 
+ *  CS50x final project winter/spring 2014  with Launch Code
+ *
+ *  copyright 2015 Robert Clark  et al.
+ *
+ **************************************************************************/
+        
+    // render header
+    require('../template/header.php');
+    
+    echo "<br /><br /><br /><br />";
+
+    
+    echo "<p class='gentext lilmar'>Submissions for 2014 have been suspended, ".
+         "submissions for 2015 will ".
+         "begin in Febuary, contact <a href='reddit?usr=ebobtron'> ebobtron</a> ".
+         "for more details.</p>";
+
+    echo "<br /><br /><br /><br />";
+
+    // render footer
+    require('../template/footer.php');
+
+
+    /*   last edit: 01/21/2015  ebt   */   
+?>
