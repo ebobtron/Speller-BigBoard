ad0b13a04e719deca91bc35f2f27535c0287241d
ebobtron <erobclark@att.net>
2015-01-24 01:13:13 -0600

    general update and rework for 2015

--- public/403.php
+++ public/403.php
@@ -1,9 +1,24 @@
+<?php
+/**
+  * 403.php  -- forbidden  --
+  *
+  * copyright 2015 Robert Clark aka ebobtron, et al.
+  *
+  * an expansion of my edX.org 
+  * CS50x final project winter/spring 2014  with Launch Code
+  *
+  ***************************************************************/
+  
+    $body = "<div class='gentext'>".
+            "<h2> Sorry this is forbidden. &nbsp;Shame on you.</h2>".
+            "</div>";     
 
-    <!-- this is forbidden  403.php -->
-    <?php require("../template/header.php"); ?>
 
-    <div class="gentext">
-    <h2> Sorry this is forbidden</h2>
-    </div> 
-
-    <?php require("../template/footer.php"); ?>
+    require("../template/header.php");
+    
+    echo $body;
+    
+    require("../template/footer.php");
+ 
+    // last edit: 
+?>
