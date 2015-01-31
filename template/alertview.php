2412b145699f3b7216d33f05f449787c3326ce07
ebobtron <erobclark@att.net>
2015-01-30 23:16:16 -0600

    submission bug issue #47 corrected

--- template/alertview.php
+++ template/alertview.php
@@ -1,17 +1,28 @@
-
+<!--
+*
+*   alertview.php  -- alert template to display alerts --
+*
+*   copyright 2015 Robert Clark(aka ebobtron), et al.
+*
+*   an expansion of my edX.org CS50x final project
+*    winter/spring 2014  with Launch Code
+********************************************************** -->  
 
 <div class="gentext">
 
-    <?=$mesString?><br />
+    <?= $mesString ?>
     <p class="center">
+    
+    <br /><br />
+
     <a href="show.php" class="head-links" style="font-size:19px">
        back to leader board</a>
     
-    <?php if($contarget !== null): ?>
+    <?php if($conTarget != null): ?>
        
-       &nbsp;&nbsp;&nbsp;...&nbsp;&nbsp;&nbsp; 
+       &nbsp; &nbsp; &nbsp;...&nbsp; &nbsp; &nbsp; 
     
-    <a href="<?=$contarget?>" class="head-links" style="font-size:22px">
+    <a href="<?= $conTarget ?>" class="head-links" style="font-size:22px">
        continue with submission</a>
     
     <?php else: ?>
@@ -22,3 +33,5 @@
     <br />
     <br />
 </div>
+
+<!--  last edit: 01/30/2015  ebt    -->
