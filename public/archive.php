5b3d21565695a77eda4faf6d795a189cbeb3652f
ebobtron <erobclark@att.net>
2015-02-01 23:30:35 -0600

    archive view and controller for 2014 lists

--- public/archive.php
+++ public/archive.php
@@ -1,28 +1,78 @@
 <?php
-/**
-  * archive.php  -- archive controller --
-  *
-  * copyright 2015 Robert Clark(aka ebobtron), et al.
-  *
-  * an expansion of my edX.org 
-  * CS50x final project winter/spring 2014  with Launch Code
-  *
-  ***************************************************************/ 
+/*
+*   archive.php  -- archive controller --
+*
+*   copyright 2015 Robert Clark(aka ebobtron), et al.
+*
+*   an expansion of my edX.org CS50x final project
+*   winter/spring 2014  with Launch Code
+***************************************************************/ 
 
     error_reporting(0); // E_ALL | 0
     
-    # require('../include/helfun.php');
-   
+    /*  getCSVTextRows() 
+     *  helper function to crate arrays from text files
+     ***************************************************/
+    function getCSVTextRows($filename, $year)
+    {
+        $path = '../include/archive'.$year.'/';
+        
+        $fileHandle = fopen($path.$filename.'.txt', 'r') or die("can't open file");
+        while(($data = fgetcsv($fileHandle, 1000, ",")) !== FALSE)
+        {
+            if($data[0])
+            {
+                $rows [] = $data;
+            }   
+        }
+        fclose($fileHandle);
+        return $rows;
+    }
+    
+    if(!isset($_GET['year']))
+    {
+        $_GET['year'] = 2014; 
+    }
+    
+    if(!isset($_GET['grp']))
+    {
+        $_GET['grp'] = 0;
+        $listshown = 'all submissions 2014';
+        $otherOne = 'Launch Code 2014';
+        $otherTwo = 'edX.org 2014';
+        $otherLinkOne = 1; 
+        $otherLinkTwo = 3;
+    }
+    elseif($_GET['grp'] == 1)
+    {
+        $listshown = 'Launch Code 2014';
+        $otherOne = 'all submissions 2014';
+        $otherTwo = 'edX.org 2014';
+        $otherLinkOne = 0; 
+        $otherLinkTwo = 3;
+    }
+    elseif($_GET['grp'] == 3)
+    {
+        $listshown = 'edX.org 2014';
+        $otherOne = 'Launch Code 2014';
+        $otherTwo = 'all submissions 2014';
+        $otherLinkOne = 1; 
+        $otherLinkTwo = 0;
+    }
+    else
+    {
+        header('location: archive');
+    }
+    
+    $rows = getCSVTextRows('rows_'.$_GET['grp'], $_GET['year']);
+    
     // render the archive page
     
     include('../template/header.php');
        
-        if(!include('../template/archive.php'))
-        {
-            include('../template/construction.php');
-        }
+    include('../template/archiveView.php');
         
     include('../template/footer.php');
     
-    // last edit: 01/24/2015  ebt
+    // last edit: 02/01/2015  ebt
 ?>
---
+++ template/archiveView.php
@@ -0,0 +1,88 @@
+<!--
+*
+*   archiveView.php   -- archive View --
+*
+*   copyright 2015 Robert Clark(aka ebobtron), et al.
+*
+*   an expansion of my edX.org CS50x final project
+*   winter/spring 2014  with Launch Code
+*************************************************************** -->
+<div class="div-table lilmar">
+    The tables below are the lists of submissions from last years CS50x offered by
+    edX.org. &nbsp;There are three lists: all submissons, those from a study group
+    sposered in Saint Louis, Missouri U.S. by Lauch Code and a general list of
+    submissions from others taking the CS50x course online world wide. 
+</div>
+
+<div class="normar">
+    <a name="pagetop" class="link"><?=$listshown?></a> &nbsp;
+    | &nbsp;
+    <a href="archive?grp=<?=$otherLinkOne?>" class="link"><?=$otherOne?></a> &nbsp;
+    | &nbsp;
+    <a href="archive?grp=<?=$otherLinkTwo?>" class="link"><?=$otherTwo?></a>
+</div>
+
+<div class="div-table">
+
+  <h4 style="margin:0px">all times in seconds</h4>
+  
+  <div class="div-col left b" style="width:20%">&nbsp; &nbsp; name</div>
+  <div class="div-col b" style="width:9%">total</div>
+  <div class="div-col b" style="width:9%">load</div>
+  <div class="div-col b" style="width:9%">check</div>
+  <div class="div-col b" style="width:9%">size</div>
+  <div class="div-col b" style="width:9%">unload</div>
+  <div class="div-col b" style="width:12%">heap</div>
+  <div class="div-col b left" style="width:13%">&nbsp;&nbsp;&nbsp;type</div> 
+
+  <!-- the clear:both stops the following elements from wrapping around
+       the last div above the efect is a new line   -->
+  <div style="clear:both;margin:0"></div>
+</div>
+
+<div class="d-sub">
+
+    <? $loop = 0; ?>
+    <? $class = null; ?>     
+    <? foreach($rows as $row): ?>
+    <? if($loop % 2 == 0): ?>
+       <? $class = ' row'; ?> 
+    <? else: ?>
+       <? $class = null; ?>
+    <? endif; ?>
+
+    <div class="div-col<?=$class?> left" style="width:20%">
+      <a href="comment?comment=<?=$row[0]?>" class="name"
+         title="click for user comments or reddit overview">&nbsp;
+         <?=$row[0]?></a></div>
+
+    <div class="div-col<?=$class?>" style="width:9%">
+        <? printf('%0.4f', $row[1]) ?></div>
+
+    <div class="div-col<?=$class?>" style="width:9%">
+        <? printf('%0.4f', $row[2]) ?></div>
+
+    <div class="div-col<?=$class?>" style="width:9%">
+        <? printf('%0.4f', $row[3]) ?></div>
+
+    <div class="div-col<?=$class?>" style="width:9%">
+        <? printf('%0.4f', $row[4]) ?></div>
+
+    <div class="div-col<?=$class?>" style="width:9%">
+        <? printf('%0.4f', $row[5]) ?></div>
+
+    <div class="div-col<?=$class?> right" style="width:12%">
+        <? printf('%0.4f MB', $row[6]) ?></div>
+
+    <div class="div-col<?=$class?> left" style="width:13%">&nbsp;
+        <?=$row[7]?></div>
+
+    <div style="clear:both"></div>
+
+    <?  $loop++;
+        endforeach; ?>
+</div>
+<br />
+
+
+<!-- last edit: 02/01/2015  ebt -->
