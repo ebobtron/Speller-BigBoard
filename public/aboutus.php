b57406cfb1eeb0a572ccccd809127b344f7d8bec
ebobtron <erobclark@att.net>
2015-02-03 12:28:31 -0600

    disabled header details for last years groups, use generic header

--- public/aboutus.php
+++ public/aboutus.php
@@ -1,23 +1,22 @@
 <?php
-/**
-  * aboutus.php   -- about us controler  --
-  *
-  * copyright 2015 Robert Clark(aka ebobtron) et al.
-  *
-  * an expansion of my edX.org 
-  * CS50x final project winter/spring 2014  with Launch Code
-  *
-  ***************************************************************/
+/*
+*   aboutus.php   -- about us controler  --
+*
+*   copyright 2015 Robert Clark(aka ebobtron) et al.
+*
+*   an expansion of my edX.org CS50x final project
+*   winter/spring 2014  with Launch Code
+***************************************************************/
 
     require '../include/helfun.php';
     
     // get group keys from tileString array
-    $keys = array_keys($titleString);
+    #$keys = array_keys($titleString);
     
     // set the group description strings for this page
-    $title = $titleString[$keys[1]];
-    $head = $headString[$keys[1]];
-    $link = $linkString[$keys[1]];
+    #$title = $titleString[$keys[1]];
+    #$head = $headString[$keys[1]];
+    #$link = $linkString[$keys[1]];
  
     // render header
     require('../template/header.php');
@@ -28,5 +27,5 @@
     // render footer
     require('../template/footer.php');
     
-    // last edit: 01/24/2015  ebt
+    // last edit: 02/03/2015  ebt
 ?>
