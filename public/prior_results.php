0f57d8b41d54e05550435744386b1154a2e723be
ebobtron <erobclark@att.net>
2015-02-14 00:41:19 -0600

    add files for 2014 charts

---
+++ public/prior_results.php
@@ -0,0 +1,30 @@
+<?php 
+/*
+*
+*  prior_results.php   -- prior year results controler -- 
+* 
+*  copyright 2015 Robert Clark(aka ebobtron), et al.
+*
+*  an expansion of my edX.org CS50x final project
+*  winter/spring 2014  with Launch Code
+******************************************************/
+    
+    error_reporting(E_ALL); // E_ALL | 0
+    
+    // body of php here
+    
+    // render header
+    require("../template/header.php");
+    
+    // render links
+    #require("../template/links.php");
+
+    // render template
+    require("../template/prior_r_view.php");
+
+    // render footer
+    require("../template/footer.php");
+
+    // last edited:  02/13/2015   ebt
+?>
+ 
---
+++ template/prior_r_view.php
@@ -0,0 +1,43 @@
+<!--
+*
+*   prior_r_view.php  * prior results view *
+*
+*   copyright 2015 Robert Clark(aka ebobtron), et al.
+*
+*   an expansion of my edX.org CS50x final project
+*    winter/spring 2014  with Launch Code
+********************************************************** -->
+
+
+<div style="margin:30px">
+<h3>2014 Results: If your into charts?</h3>
+   
+  <a href="http://speller-leaderboard.freehostia.com/public/img/subs_all_2014.jpg"> 
+  
+  <img src="img/subs_all_2014.jpg" alt="2014 chart" 
+       style="height:30%;width:30%;padding:15px;float:left" title="click me to make me bigger" />
+       
+  </a>
+  
+<p style="padding:2px;font-size:1.1em">Last year 135 submissions were posted to the
+board.&nbsp; Many of those that submitted posted several times so the numbers are skewed in some fashion that only a statistics lover might appreciate.</p>
+
+  <a href="http://speller-leaderboard.freehostia.com/public/img/subs_all_2014b.jpg"> 
+  
+  <img src="img/subs_all_2014b.jpg" alt="2014 chart" 
+       style="height:30%;width:30%;padding:15px;float:right" title="click me to make me bigger" />
+       
+  </a>
+
+<p style="padding:2px;font-size:1.1em">       
+The charts shown here represent the spread of the 135 submissions across total average time up to 1 second.&nbsp; To see where you fall in the spread compared to last year find your chuck of time in 0.05 second intervals at the bottom of the chart.&nbsp; The rise and fall of the first peak makes up 46 percent of the submissions, a reasonably tight group.
+<br><br>
+Times above 1 second are not shown.
+<p style="padding-left:2px">charts prepared using
+   <a href="https://www.openoffice.org/license.html" style="text-decoration:none;">
+      "OpenOffice 4"</a>
+</div>
+
+
+
+<!--  last edit: 02/14/2015  ebt    -->
