21ca17bacd9fe9e03ae22236af9c2e67b25347cc
ebobtron <erobclark@att.net>
2015-08-26 22:43:09 -0500

    error reporting enhancement

---
+++ template/error.php
@@ -0,0 +1,17 @@
+<!-- 
+    error.php  ** error view template **
+    last edited: 03/17/2015 ebt
+ -->
+ 
+<div class="gentext" style="height:300px">
+<br />
+<br />
+<h2>Error: <?= $errortype ?></h2>
+
+<?= $error ?>
+
+<br />
+<br />
+
+</div>
+
